f500521d8f15f2c3219cae1e7a89729b1c9e9c1a
Travis Roberts <travis.roberts2@gmail.com>
2018-02-20 00:01:15 -0600

    heroku

--- models/index.js
+++ models/index.js
@@ -12,6 +12,7 @@ if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  console.log(sequelize)
 }
 
 fs
